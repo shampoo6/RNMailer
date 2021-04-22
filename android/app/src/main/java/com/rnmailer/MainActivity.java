bed0c9917447d6fdc109600daa3980a77c9ef2e7
shampoo6 <shampoo6@163.com>
2021-04-22 14:55:15 +0800

    添加MainActivity的状态

--- android/app/src/main/java/com/rnmailer/MainActivity.java
+++ android/app/src/main/java/com/rnmailer/MainActivity.java
@@ -1,8 +1,42 @@
 package com.rnmailer;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.rnmailer.constants.ActivityStatus;
 
 public class MainActivity extends ReactActivity {
+    public static String MAIN_STATUS_KEY = "MAIN_STATUS_KEY";
+    public static String STATUS = "STATUS";
+
+    private SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sp = getSharedPreferences(MAIN_STATUS_KEY, 0);
+        sp.edit().putString(STATUS, ActivityStatus.Front.name()).apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sp.edit().putString(STATUS, ActivityStatus.Background.name()).apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sp.edit().putString(STATUS, ActivityStatus.Front.name()).apply();
+    }
+
+    @Override
+    protected void onDestroy() {
+        sp.edit().putString(STATUS, ActivityStatus.ShutDown.name()).apply();
+        super.onDestroy();
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
---
+++ android/app/src/main/java/com/rnmailer/constants/ActivityStatus.java
@@ -0,0 +1,7 @@
+package com.rnmailer.constants;
+
+public enum ActivityStatus {
+    Front, // 前台
+    Background, // 后台
+    ShutDown // 关闭
+}
